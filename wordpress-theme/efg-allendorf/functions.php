7ff82ab6b40b55dc9b61849210bb178129d6cf5e
Levi Kirr <info@kirrovo.marketing>
2026-09-10 15:37:41 +0200

    Glaubensbereich klarer und luftiger gestalten

--- wordpress-theme/efg-allendorf/functions.php
+++ wordpress-theme/efg-allendorf/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Direktaufruf verhindern.
 }
 
-define( 'EFGA_VERSION', '1.0.13' );
+define( 'EFGA_VERSION', '1.0.14' );
 
 /* ════════════════════════════════════════════════════════
    1. THEME-SETUP
