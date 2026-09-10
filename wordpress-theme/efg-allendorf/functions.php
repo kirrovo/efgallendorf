33c75205f6426574df26975b356547494447bece
Levi Kirr <info@kirrovo.marketing>
2026-09-10 18:49:59 +0200

    Add fading images to women men and senior group cards

--- wordpress-theme/efg-allendorf/functions.php
+++ wordpress-theme/efg-allendorf/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Direktaufruf verhindern.
 }
 
-define( 'EFGA_VERSION', '1.0.33' );
+define( 'EFGA_VERSION', '1.0.34' );
 
 /* ════════════════════════════════════════════════════════
    1. THEME-SETUP
--- wordpress-theme/efg-allendorf/template-parts/gruppe-card.php
+++ wordpress-theme/efg-allendorf/template-parts/gruppe-card.php
@@ -12,7 +12,7 @@ $schedule = efga_get( 'efga_schedule' );
 $short    = efga_get( 'efga_short', wp_trim_words( get_the_excerpt(), 20 ) );
 $card_bild = '';
 $card_slug = get_post_field( 'post_name' );
-if ( in_array( $card_slug, array( 'gottesdienst', 'glv', 'bibelstunde', 'hauskreise', 'kindergottesdienst', 'knallerbsen', 'wilde-fuechse', 'biblischer-unterricht', 'crossroad', 'kreisjugend' ), true ) ) {
+if ( in_array( $card_slug, array( 'gottesdienst', 'glv', 'bibelstunde', 'hauskreise', 'kindergottesdienst', 'knallerbsen', 'wilde-fuechse', 'biblischer-unterricht', 'crossroad', 'kreisjugend', 'kaffee-und-muesli', 'frauengebetskreis', 'maennertreffen', 'seniorenarbeit' ), true ) ) {
 	$card_bild_slug = 'kreisjugend' === $card_slug ? 'crossroad' : $card_slug;
 	$card_bild = has_post_thumbnail()
 		? get_the_post_thumbnail_url( null, 'medium_large' )
